4269941f666ed98fcc1e534bcbfab8084c3f0eab
TrackyRaul <raul.farkas19@gmail.com>
2019-12-18 18:16:16 +0100

    Button disappear when users disconnect

--- src/ChatController.java
+++ src/ChatController.java
@@ -2,6 +2,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -22,18 +23,23 @@ public class ChatController {
     }
 
     public void loadUsersButtons(ArrayList<String> users) {
-        for (String s : users) {
-            Button b = new Button(s);
-            b.setOnAction(new EventHandler<ActionEvent>(){
-                @Override
-                public void handle(ActionEvent actionEvent) {
-                    String name = s;
-                    System.out.println(name);
-                }
-            });
-            b.setPrefSize(200, 30);
-            usersList.getChildren().add(b);
-        }
+        Platform.runLater(() -> {
+            usersList.getChildren().clear();
+            for (String s : users) {
+                Button b = new Button(s);
+                b.setOnAction(new EventHandler<ActionEvent>(){
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        String name = s;
+                        System.out.println(name);
+
+                    }
+                });
+                b.setPrefSize(200, 30);
+                usersList.getChildren().add(b);
+            }
+
+        });
 
     }
 }
